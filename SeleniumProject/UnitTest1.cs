90556a7e57e85e1b987d9313eda79d9480c9fd62
Ekaterina Veselkova <vslkve@gmail.com>
2022-02-08 21:28:02 +0300

    add project

---
+++ SeleniumProject/UnitTest1.cs
@@ -0,0 +1,33 @@
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumProject
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new FirefoxDriver(@"C:\distr");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://music.yandex.ru";
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
