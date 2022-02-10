7274a93b7ab2656ddd402c132d06ce4263555c70
Ekaterina Veselkova <vslkve@gmail.com>
2022-02-10 20:46:46 +0300

    Задание 3

---
+++ 3/SeleniumProject/UnitTest1.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace SeleniumTests
+{
+    public class TestCase
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        protected WebDriverWait wait;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            verificationErrors = new StringBuilder();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void Test()
+        {
+            OpenHomePage();
+            Login();
+            ClickAnyButton();
+            Logout();
+        }
+
+        private void Logout()
+        {
+            driver.FindElement(By.XPath("//td[@id='sidebar']/div[2]/a[5]/i")).Click();
+        }
+
+        private void ClickAnyButton()
+        {            
+            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Appearence'])[1]/following::span[2]")).Click();
+            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Currencies'])[1]/following::span[2]")).Click();
+            driver.FindElement(By.XPath("//td[@id='sidebar']/div[2]/a[2]/i")).Click();
+        }
+
+        private void Login()
+        {
+            driver.FindElement(By.Name("username")).Click();
+            driver.FindElement(By.Name("username")).Clear();
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).Click();
+            driver.FindElement(By.Name("password")).Clear();
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl("http://localhost/litecart/admin/login.php");
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
