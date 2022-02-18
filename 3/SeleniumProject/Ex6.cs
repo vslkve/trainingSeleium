a63188557afcaa17c5a75d12f1ca7ea2a071c92b
Ekaterina Veselkova <vslkve@gmail.com>
2022-02-18 23:34:41 +0300

    Update Ex6.cs

--- 3/SeleniumProject/Ex6.cs
+++ 3/SeleniumProject/Ex6.cs
@@ -5,6 +5,8 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SeleniumTests
 {
@@ -17,14 +19,16 @@ namespace SeleniumTests
         protected WebDriverWait wait;
         
 
-
         [SetUp]
         
         public void SetupTest()
         {
             driver = new FirefoxDriver();
             verificationErrors = new StringBuilder();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            
+
         }
 
         [Test]
@@ -32,143 +36,31 @@ namespace SeleniumTests
         {
             OpenHomePage();
             Login();
-            //Appearence            
-            driver.FindElement(By.XPath("//li[@id='app-']/a/span[2]")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-template']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-logotype']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Catalog
-            driver.FindElement(By.LinkText("Catalog")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-catalog']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-product_groups']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-option_groups']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-manufacturers']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-suppliers']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-delivery_statuses']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-sold_out_statuses']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-quantity_units']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-csv']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Countries
-            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='CSV Import/Export'])[1]/following::span[2]")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Currencies
-            driver.FindElement(By.LinkText("Currencies")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Customers
-            driver.FindElement(By.LinkText("Customers")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-customers']/a")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-csv']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-newsletter']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Geo zones
-            driver.FindElement(By.LinkText("Geo Zones")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Langueges
-            driver.FindElement(By.LinkText("Languages")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-languages']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-storage_encoding']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Modules
-            driver.FindElement(By.LinkText("Modules")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-jobs']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-customer']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-shipping']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-payment']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-order_total']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-order_success']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-order_action']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Orders
-            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Order Action'])[1]/following::span[2]")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-orders']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-order_statuses']/a/span")).Click();
-            Assert.True(HeaderExist());
-            //Pages
-            driver.FindElement(By.LinkText("Pages")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Reports
-            driver.FindElement(By.LinkText("Reports")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-monthly_sales']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-most_sold_products']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-most_shopping_customers']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Settings
-            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Most Shopping Customers'])[1]/following::span[2]")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.LinkText("Store Info")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-defaults']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-general']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-listings']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-images']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-checkout']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-advanced']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-security']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Slides
-            driver.FindElement(By.LinkText("Slides")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Tax
-            driver.FindElement(By.LinkText("Tax")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-tax_classes']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-tax_rates']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Translations
-            driver.FindElement(By.LinkText("Translations")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-search']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-scan']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-csv']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //Users
-            driver.FindElement(By.LinkText("Users")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            //vQmods
-            driver.FindElement(By.LinkText("vQmods")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
-            driver.FindElement(By.XPath("//li[@id='doc-vqmods']/a/span")).Click();
-            Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
+            Menu();          
+
         }
 
+        public void Menu()
+        {
+            List<IWebElement> elementsMenu = driver.FindElements(By.Id("app-")).ToList();
+            for (int i=0; i < elementsMenu.Count; i++)
+            {
+                var elements = driver.FindElements(By.Id("app-")).ToList()[i];
+                elements.Click();
+                Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
+                if(AreElementsPresent(By.ClassName("docs")))
+                {
+                    List<IWebElement> elementUnderMenu = driver.FindElements(By.ClassName("docs")).ToList();
+                    for (int j = 0; j < elementUnderMenu.Count; j++)
+                    {
+                        elementUnderMenu[j].Click();
+                        Assert.True(AreElementsPresent(By.XPath("//td[@id='content']/h1")));
+                    }
+                }
+            }
+        }
+
+        
 
         public By ExistHeader { get { return By.XPath("//td[@id='content']/h1"); } }
 
@@ -181,6 +73,8 @@ namespace SeleniumTests
         {
             return driver.FindElements(locator).Count > 0;
         }
+
+       
 
         private void Logout()
         {
