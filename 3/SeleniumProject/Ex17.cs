c07a88d3a7b28479bef957bd8d51ef3bb81d5819
Ekaterina Veselkova <vslkve@gmail.com>
2022-03-10 02:22:19 +0300

    Ex17

---
+++ 3/SeleniumProject/Ex17.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+using SeleniumExtras.WaitHelpers;
+
+namespace SeleniumTests
+{
+    public class Ex17
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private bool acceptNextAlert = true;
+        protected WebDriverWait wait;
+
+
+        [SetUp]
+
+        public void SetupTest()
+        {
+            //driver = new FirefoxDriver();
+            driver = new ChromeDriver();
+            verificationErrors = new StringBuilder();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+        }
+
+        [Test]
+        public void Test()
+        {
+            OpenHomePage();
+            Login();
+            OpenCategoryPage();
+            OpenAndCheck();
+        }
+
+        public void OpenAndCheck()
+        {
+            var products = driver.FindElements(By.XPath("//*[@id='content']/form/table/tbody/tr/td[3]/a"));
+            for (int i = 0; i < products.Count; i++)
+            { 
+                var item = driver.FindElements(By.XPath("//*[@id='content']/form/table/tbody/tr/td[3]/a"));
+                item[i].Click();
+                var logs = driver.Manage().Logs.GetLog(LogType.Browser);
+                foreach (var log in logs)
+                {
+                    Console.WriteLine(log);
+                    Assert.IsEmpty(log.Message);
+                }
+                OpenCategoryPage();
+            }
+
+        }
+
+
+        private void Login()
+        {
+            driver.FindElement(By.Name("username")).Click();
+            driver.FindElement(By.Name("username")).Clear();
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).Click();
+            driver.FindElement(By.Name("password")).Clear();
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        private void OpenCategoryPage()
+        {
+            driver.Navigate().GoToUrl("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl("http://localhost/litecart/admin/login.php");
+        }
+
+        public bool AreElementsPresent(By locator)
+        {
+            return driver.FindElements(locator).Count > 0;
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
+    }
+}
